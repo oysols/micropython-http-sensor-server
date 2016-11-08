c1e8eb496276f890d1c86d0f954379674c9e2c60
Øystein Olsen <mail@oysteinolsen.com>
2016-11-08 01:41:22 +0100

    Improve comments

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import temp
 import network
 
+# Response sent to web-client
 CONTENT = b"""\
 HTTP/1.0 200 OK
 
@@ -13,7 +14,9 @@ The temperature is {}.{} C.
 Best regards,
 WiPy
 """
+
 def answer_request(client_socket, client_addr):
+    """Answer request with temperature measurement"""
     req = client_socket.readline()
     print("Request:")
     print(req)
@@ -25,6 +28,7 @@ def answer_request(client_socket, client_addr):
     temperature, temperature_decimal = temp.get_temp(10000,0)
     alive = time.time() - 473385595
     data = CONTENT.format(alive, temperature, temperature_decimal)
+    # Block to make sure all data is written before closing socket.
     client_socket.setblocking(True)
     client_socket.write(data)
     client_socket.setblocking(False)
@@ -37,22 +41,27 @@ def main():
 
     addr = ai[0][-1]
     s.bind(addr)
+    s.listen(0)
 
-    s.listen(5)
+    # Do not block to allow main loop to proceed
+    # while waiting for request.
     s.setblocking(False)
-    print("Listening, connect your browser to http://<this_host>:80/")
 
     while True:
+        """Main loop"""
         client_socket, client_addr = None, None
-        try:
+        try: # Will fail if allready accepted socket
             client_socket, client_addr = s.accept()
         except:
             pass
+
         if client_socket:
-            try:
+            try: # Improve stability
                 answer_request(client_socket, client_addr)
             except:
                 try:
+                    # Closing socket will fail if
+                    # socket is allready closed.
                     client_socket.close_socket()
                 except:
                     pass
